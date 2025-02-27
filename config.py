c1bf2e37668f6af612db903f4bd32bd647a7bc42
Shubha0200 <rb5028432@gmail.com>
2025-02-28 01:07:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,6 @@ DB_NAME = os.environ.get("DATABASE_NAME", "shubhashankar246")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002262591479"))
-JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
