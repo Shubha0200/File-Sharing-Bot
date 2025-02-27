3ff81bd60d68faffa281bca19edda2699c534b4d
Shubha0200 <rb5028432@gmail.com>
2025-02-28 00:22:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -70,29 +70,54 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "False").lower
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+#(©)CodeXBotz
 
-ADMINS.append(OWNER_ID)
-ADMINS.append(1782088532)
+import os
+import logging
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
 
-LOG_FILE_NAME = "filesharingbot.txt"
+load_dotenv()
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-    datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7532458641:AAHJgXb9LoeVbtPMRlLhHe-v4F-V_176gJ4")
+
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "21459238"))
+
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "0cef1924d139587654bc18402d2b512d")
+
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002448726887"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "1782088532"))
+
+#Port
+PORT = os.environ.get("PORT", "8080")
+
+#Database 
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://rb5028432:<db_password>@cluster0.a3wsp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "mongodb+srv://rb5028432:@cluster0.a3wsp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002343164262")),
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002262591479")),
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_3", "-1002383034992")),
+JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", "False").lower() == "true"
+
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_PIC = os.environ.get("START_PIC", "")
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "Hello {first}\n\nI can store private files in a specified channel, and other users can access them via a special link."
 )
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
+
 
 #(©)CodeXBotz
 
@@ -122,12 +147,13 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1782088532"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shubhashankar246:nEWTylzLZpiuysRh@cluster0.kpecd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_UR = os.environ.get("DATABASE_URL", "mongodb+srv://shubhashankar246:nEWTylzLZpiuysRh@cluster0.kpecd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "shubhashankar246")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002343164262"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002262591479"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002383034992"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -136,13 +162,13 @@ START_PIC = os.environ.get("START_PIC","")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "1782088532").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -151,18 +177,18 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 # Auto delete time in seconds.
-AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "0"))
+AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "1800"))
 AUTO_DELETE_MSG = os.environ.get("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
 AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
 
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1782088532)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
